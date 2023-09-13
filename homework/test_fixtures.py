2531a26ca0e3744eae2eb587e2c55a9489c610f8
Aleksandra Kashina <Borland3711@gmail.com>
2023-09-13 23:20:19 +0300

    Homework with applying Pytest

---
+++ homework/test_fixtures.py
@@ -0,0 +1,32 @@
+"""
+Сделайте разные фикстуры для каждого теста, которые выставят размеры окна браузера
+"""
+import pytest
+from selene import browser, have
+from selene.support.shared import browser, config
+
+
+@pytest.fixture()
+def windows_size_desktop():
+    browser.config.window_width = 1920
+    browser.config.window_height = 1080
+
+
+@pytest.fixture()
+def windows_size_mobile():
+    browser.config.window_width = 360
+    browser.config.window_height = 640
+
+
+def test_github_desktop(windows_size_desktop):
+    browser.open('https://github.com')
+    browser.element('.HeaderMenu-link--sign-in').click()
+    browser.element('.auth-form-header').should(have.exact_text('Sign in to GitHub'))
+
+
+def test_github_mobile(windows_size_mobile):
+    browser.open('https://github.com')
+    browser.element('.flex-1 [aria-label="Toggle navigation"]').click()
+    browser.element('a.HeaderMenu-link--sign-in').click()
+    browser.element('.auth-form-header').should(have.exact_text('Sign in to GitHub'))
+
---
+++ homework/test_parametrize.py
@@ -0,0 +1,30 @@
+"""
+Переопределите параметр с помощью indirect параметризации на уровне теста
+"""
+import pytest
+from selene import browser, have
+from selene.support.shared import browser, config
+
+
+
+@pytest.fixture(params=[(1920, 1080), (360, 640)])
+def browser_size(request):
+    browser.config.window_width = request.param[0]
+    browser.config.window_height = request.param[1]
+
+
+@pytest.mark.parametrize("browser_size", [(1920, 1080)], ids=['1920 * 1080'], indirect=True)
+def test_github_desktop(browser_size):
+    browser.open('https://github.com/')
+    browser.element('.HeaderMenu-link--sign-in').click()
+    browser.element('.auth-form-header').should(have.exact_text('Sign in to GitHub'))
+
+
+@pytest.mark.parametrize("browser_size", [(360, 640)], ids=['360 * 640'], indirect=True)
+def test_github_mobile(browser_size):
+    browser.open('https://github.com')
+    browser.element('.flex-1 [aria-label="Toggle navigation"]').click()
+    browser.element('a.HeaderMenu-link--sign-in').click()
+    browser.element('.auth-form-header').should(have.exact_text('Sign in to GitHub'))
+
+
